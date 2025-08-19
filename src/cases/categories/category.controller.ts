b40bd3a7bdae06097d9eb36383d943b2eccab438
Adrieli Balbinot <adrielibalbinot1@.com>
2025-08-18 23:07:21 -0300

    Aula 18/08/2025

--- src/cases/categories/category.controller.ts
+++ src/cases/categories/category.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, ParseUUIDPipe } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, ParseUUIDPipe, Post, Put } from "@nestjs/common";
 import { Category } from "./category.entity";
-import { CategoryService } from './category.service';
+import { CategoryService } from "./category.service";
+
 
 @Controller('categories')
 export class CategoryController {
@@ -13,15 +14,46 @@ export class CategoryController {
     }
 
     @Get(':id')
-    findById(@Param('id', ParseUUIDPipe) id: string): Promise<Category> {
-        return this.service.findById(id);
+    async findById(@Param('id', ParseUUIDPipe) id: string): Promise<Category | null> {
+        const found = this.service.findByID(id);
+
+        if (!found) {
+            throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
+        }
+
+        return found;
     }
 
-    save(category: Category): Promise<Category> {
+    @Post()
+    create(@Body() category: Category): Promise<Category> {
         return this.service.save(category);
     }
 
-    remove(): Promise<void> {
+    @Put(':id')
+    async update(@Param('id', ParseUUIDPipe) id: string, @Body() category: Category): Promise<Category> {
+
+        const found = await this.service.findByID(id);
+
+        if (!found) {
+            throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
+        }
+
+        category.id = id;
+
+        return this.service.save(category);
+    }
+
+    @Delete(':id')
+    @HttpCode(204)
+    async remove(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+
+        const found = await this.service.findByID(id);
+
+        if (!found) {
+            throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
+        }
+
+        return this.service.remove(id);
 
     }
 }
