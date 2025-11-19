1340c6c1171948209aa9c0effcb5509fa4fbf92a
Adrieli Balbinot <adrielibalbinot1@.com>
2025-11-18 21:25:53 -0300

    Implementado order

--- src/cases/orders/order.module.ts
+++ src/cases/orders/order.module.ts
@@ -1,14 +1,20 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Order } from "./entities/order.entity";
-import { OrderController } from "./controllers/order.controller";
+
 import { OrderItem } from "./entities/order-item.entity";
-import { OrderItemController } from "./controllers/order-item.controller";
 import { OrderService } from "./service/order.service";
 import { OrderItemService } from "./service/order-item.service";
+import { CustomerModule } from "../customers/customer.module";
+import { OrderItemController } from "./controllers/order-item.controller";
+import { OrderController } from "./controllers/order.controller";
+
 
 @Module({
-    imports:[TypeOrmModule.forFeature([Order, OrderItem])],
+    imports:[TypeOrmModule.forFeature([Order, OrderItem]),
+    CustomerModule
+],
+
     providers: [OrderService, OrderItemService],
     controllers: [OrderController, OrderItemController]
 })
